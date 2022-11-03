a9f04cd2862cef9aaf4de94c5c34bb5a75ad8857
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-03 09:54:21 -0400

    Update the config script to match the overlay name

--- vf-conf.sh
+++ vf-conf.sh
@@ -4,8 +4,8 @@ echo "Creating Haxelib directory for the portage user."
 sleep 1
 mkdir -p /var/tmp/portage/.local/share/haxe/
 cp -r /usr/share/haxe/lib /var/tmp/portage/.local/share/haxe
-echo "Symlinking ltf-conf to /usr/bin."
+echo "Symlinking vf-conf to /usr/bin."
 sleep 1
-ln -s /var/db/repos/LarryTheFunker/ltf-conf.sh /usr/bin/ltf-conf
-chmod +x /usr/bin/ltf-conf
+ln -s /var/db/repos/violet-funk/ltf-conf.sh /usr/bin/vf-conf
+chmod +x /usr/bin/vf-conf
 echo "Done."

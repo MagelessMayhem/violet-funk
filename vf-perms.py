09aaf04270e8188a3cf968231297cafbea353adc
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-03 23:19:00 -0400

    Add files via upload

---
+++ vf-perms.py
@@ -0,0 +1,9 @@
+import os
+
+print("Name of directory in /usr/share/games to grant user access to?")
+given_dir = input()
+
+if os.path.exists("/usr/share/games/" + given_dir):
+    os.system("sudo chown $(whoami) /usr/share/games/" + given_dir + "/*")
+
+print("You should now own the folder.")

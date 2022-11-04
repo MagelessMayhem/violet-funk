ea254079bba41badd6c4aac21cc7afd7acc11e1b
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-03 23:35:47 -0400

    Update vf-perms.py

--- vf-perms.py
+++ vf-perms.py
@@ -4,7 +4,7 @@ print("Name of directory in /usr/share/games to grant user access to?")
 given_dir = input()
 output = Popen(["whoami"],stdout=PIPE)
 result = output.communicate()
-current_user = result[0].decode("utf-8").split()
+current_user = result[0].decode("utf-8").split()[0]
 if os.path.exists("/usr/share/games/" + given_dir):
     print("Granting ownership.")
     os.system("sudo chown " + current_user + " /usr/share/games/" + given_dir + "/*")

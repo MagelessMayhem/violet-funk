c73c0e579e9d9e3dc84c4692380d58a9b7e5ee26
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-03 23:26:34 -0400

    Update vf-perms.py

--- vf-perms.py
+++ vf-perms.py
@@ -1,10 +1,9 @@
 import os
-
+from subprocess import Popen,PIPE
 print("Name of directory in /usr/share/games to grant user access to?")
 given_dir = input()
-
-current_user = os.system("whoami")
-
+output = Popen(["whoami"],stdout=PIPE)
+current_user = output.communicate()
 if os.path.exists("/usr/share/games/" + given_dir):
     print("Granting ownership.")
     os.system("sudo chown " + current_user + " /usr/share/games/" + given_dir + "/*")

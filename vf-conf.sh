986f0774317859c30df592c52937a07bec0aa6af
MagelessMayhem <107271276+MagelessMayhem@users.noreply.github.com>
2022-11-03 20:24:43 -0400

    Remove automatic symlink
    Remove the portion of the script which symlinks itself to /usr/bin. This could have caused problems when trying to sync the overlay.

--- vf-conf.sh
+++ vf-conf.sh
@@ -10,8 +10,4 @@ echo "Creating Haxelib directory for the portage user."
 sleep 1
 mkdir -p /var/tmp/portage/.local/share/haxe/
 cp -r /usr/share/haxe/lib /var/tmp/portage/.local/share/haxe
-echo "Symlinking vf-conf to /usr/bin."
-sleep 1
-ln -s /var/db/repos/violet-funk/vf-conf.sh /usr/bin/vf-conf
-chmod +x /usr/bin/vf-conf
-echo "Done."
+echo "Done. The portage user's Haxelib directory is now /var/tmp/portage/.local/share/haxe/lib."
